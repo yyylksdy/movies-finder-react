f87fbb2386a75eb3a2b8bf8d6107b2b7b71ff40d
Ada <ada@twobulls.com>
2021-02-07 18:41:44 -0500

    finish main functionality

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchForm from '../SearchForm/SearchForm'
+import ListTable from '../Table/Table'
+
+const Home = () => (
+  <div>
+    <SearchForm />
+    <ListTable />
+  </div>
+)
+
+export default Home
